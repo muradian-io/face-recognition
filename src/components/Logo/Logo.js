12d269c5f1f33da2f193bc16e97def77cbecd083
Wael Joseph Murad <waelmuradian@outlook.com>
2019-01-29 13:56:27 -0800

    Blue Background

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Tilt from 'react-tilt'
+import face from './logo.png'
 import './Logo.css'
 const Logo = () => {
     return ( 
         <div>
             <Tilt className = "Tilt br2 ma5 shadow-2" options = {{max: 55}}style = { {height: 150,width: 150 }}>
-            <div className = "Tilt-inner tc" > Logo </div> 
+            <div className = "Tilt-inner pa4" > <img style={{paddintTop:'5px'}}alt='logo' src={face}/> </div> 
             </Tilt>
         </div>
     )
