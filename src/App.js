6da3dc7ef47fa974f5d45e83d25a7de86b05198d
Wael Joseph Murad <waelmuradian@outlook.com>
2019-01-29 14:52:19 -0800

    adding ImageLinkForm component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js'
+
+const ParticlesOptions = {
+  particles: {
+    number: {
+      value:80,
+      density:{
+        enable:true,
+        value_area:500,
+      }
+    }
+}
+}
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <Particles className='Particles'
+       params ={ParticlesOptions}
+       />
        <Navigation />
        <Logo />
+       < ImageLinkForm/>
       </div>
     );
   }
